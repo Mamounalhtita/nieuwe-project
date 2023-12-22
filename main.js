2ea348249f0bc3486a053582c81c9496802caca9
Mamounalhtita <302734233@student.rocmondriaan.nl>
2023-12-22 15:23:32 +0100

    ..

--- main.js
+++ main.js
@@ -1,18 +1,42 @@
+///let title = document.querySelector(".title");
+//let turn = "X" ;
+
+//function game(id)
+//{
+    //let element = document.getElementById(id);
+   // if (turn === "X" && element.innerHTML == "")
+    //{
+      //  element.innerHTML = "X";
+       // turn = "O";
+      //  title.innerHTML ="O";
+   // }
+   // else if(turn === "O" && element.innerHTML == ""){
+   // title.innerHTML ="O";
+   // turn = "X";
+    //title.innerHTML ="X";
+    //}
+//}
+
 let title = document.querySelector(".title");
 let turn = "X" ;
+let currentTitle = "";
+function winner(){}
 
 function game(id)
 {
     let element = document.getElementById(id);
+
+    currentTitle = title.innerHTML;
+
     if (turn === "X" && element.innerHTML == "")
     {
         element.innerHTML = "X";
         turn = "O";
-        title.innerHTML ="O";
+        title.innerHTML = "O";
     }
     else if(turn === "O" && element.innerHTML == ""){
-    title.innerHTML ="O";
-    turn = "X";
-    title.innerHTML ="X";
+        element.innerHTML = "O";
+        turn = "X";
+        title.innerHTML = currentTitle; // Use the saved title value
     }
 }
