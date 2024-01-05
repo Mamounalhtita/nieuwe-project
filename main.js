4511021d2879314ad3fbcfcf86d0cfd1a64c06e9
Mamounalhtita <302734233@student.rocmondriaan.nl>
2024-01-05 20:24:05 +0100

    nog niet af

--- main.js
+++ main.js
@@ -1,42 +1,71 @@
-///let title = document.querySelector(".title");
-//let turn = "X" ;
-
-//function game(id)
-//{
-    //let element = document.getElementById(id);
-   // if (turn === "X" && element.innerHTML == "")
-    //{
-      //  element.innerHTML = "X";
-       // turn = "O";
-      //  title.innerHTML ="O";
-   // }
-   // else if(turn === "O" && element.innerHTML == ""){
-   // title.innerHTML ="O";
-   // turn = "X";
-    //title.innerHTML ="X";
-    //}
-//}
 
 let title = document.querySelector(".title");
-let turn = "X" ;
+let turn = "X";
 let currentTitle = "";
-function winner(){}
+let squares = [];
+function end(num1,num2,num3){
+    title.innerHTML ='${squares[num1] }winner';
+    document.getElementById('item'+num1).style.background = '#000';
+    document.getElementById('item'+num2).style.background = '#000';
+    document.getElementById('item'+num3).style.background = '#000';
 
-function game(id)
-{
+    setInterval(function(){title.innerHTML +='.'},1000);
+    setTimeout(function(){location.reload()},4000)
+
+}
+function winner() {
+    for (let i = 1; i < 10; i++) {
+        squares[i] = document.getElementById("item" + i).innerHTML;
+    }
+
+    if (squares[1] == squares[2] && squares[2] == squares[3] && squares[1] != "") 
+    {
+        end(1,2,3);
+    }
+    else if (squares[4] == squares[5] && squares[5] == squares[6] && squares[5] != "") 
+    {
+        end(4,5,6);
+    }
+    else if (squares[7] == squares[8] && squares[8] == squares[9] && squares[8] != "") 
+    {
+        end(7,8,9);
+    }
+    if (squares[1] == squares[4] && squares[4] == squares[7] && squares[1] != "") 
+    {
+        end(1,4,7);
+    }
+    if (squares[2] == squares[5] && squares[5] == squares[8] && squares[5] != "") 
+    {
+        end(2,5,7);
+    }
+    if (squares[3] == squares[6] && squares[6] == squares[9] && squares[6] != "")
+     {
+        end(3,6,9);
+    }
+    if (squares[1] == squares[5] && squares[5] == squares[9] && squares[5] != "")
+     {
+        end(1,5,9);
+    }
+    if (squares[3] == squares[5] && squares[5] == squares[7] && squares[5] != "")
+     {
+        end(3,5,7);
+    }
+}
+
+function game(id) {
     let element = document.getElementById(id);
 
     currentTitle = title.innerHTML;
 
-    if (turn === "X" && element.innerHTML == "")
-    {
+    if (turn === "X" && element.innerHTML == "") {
         element.innerHTML = "X";
         turn = "O";
         title.innerHTML = "O";
-    }
-    else if(turn === "O" && element.innerHTML == ""){
+        winner();
+    } else if (turn === "O" && element.innerHTML == "") {
         element.innerHTML = "O";
         turn = "X";
         title.innerHTML = currentTitle; // Use the saved title value
+        winner();
     }
 }
